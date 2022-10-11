ae79a85e6b4237262dd38b2a914b65487384e005
CharanAbarajithan <charanabarajithan1@gmail.com>
2022-10-11 16:55:04 +1100

    testing

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3rT1NWiQobUOa4xotpLmhREtspTpKleQ",
+  authDomain: "crypto-tracker-c6c2c.firebaseapp.com",
+  projectId: "crypto-tracker-c6c2c",
+  storageBucket: "crypto-tracker-c6c2c.appspot.com",
+  messagingSenderId: "920708752123",
+  appId: "1:920708752123:web:ef5fe50503e961d6a6465e",
+};
+
+
+export default firebaseConfig;
